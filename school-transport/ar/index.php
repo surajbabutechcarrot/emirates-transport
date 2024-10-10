b141c0d6508892f33f8c67ab79db33dba1692ed0
surajbabutechcarrot <101328482+surajbabutechcarrot@users.noreply.github.com>
2024-10-10 12:16:37 +0400

    latest

--- school-transport/ar/index.php
+++ school-transport/ar/index.php
@@ -1,14 +1,13 @@
 <?php 
 $page_title = "School Trams";
-include('../includes/header.php');
+include('../../includes/header-ar.php');
 ?>
-
 <main class="inner-page">
     <!-- Banner start -->
     <section class="inner-banner school contact pt-0 d-flex align-items-center">
         <div class="container-fluid">
             <div class="banner-content text-center text-uppercase">
-                <h1 class="fw-bold text-white">School Transportation</h1>
+                <h1 class="fw-bold text-white">النقل المدرسي</h1>
                 <img class="bg" src="<?php echo $path; ?>assets/images/school-banner-bg.png" alt="About Us Banner">
             </div>
         </div>
@@ -19,24 +18,24 @@ include('../includes/header.php');
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <p class="text-gray">43 years of excellence</p>
-                    <h2 class="ttl-40 fw-bold text-black text-uppercase pb-4">Our services in numbers</h2>
+                    <p class="text-gray">43 عامًا من التميز</p>
+                    <h2 class="ttl-40 fw-bold text-black text-uppercase pb-4">خدماتنا بالأرقام</h2>
                 </div>
             </div>
         </div>
         <div class="container-fluid px-5 pt-5 number-sec">
             <div class="row">
                 <div class="col-lg-3 col-sm-6 col-6 p-lg-0">
-                    <img src="<?php echo $path; ?>assets/images/numbers-1.png">
+                    <img src="<?php echo $path; ?>assets/images/numbers-1-ar.png">
                 </div>
                 <div class="col-lg-3 col-sm-6 col-6 p-lg-0">
-                    <img src="<?php echo $path; ?>assets/images/numbers-2.png">
+                    <img src="<?php echo $path; ?>assets/images/numbers-2-ar.png">
                 </div>
                 <div class="col-lg-3 col-sm-6 col-6 p-lg-0">
-                    <img src="<?php echo $path; ?>assets/images/numbers-3.png">
+                    <img src="<?php echo $path; ?>assets/images/numbers-3-ar.png">
                 </div>
                 <div class="col-lg-3 col-sm-6 col-6 p-lg-0">
-                    <img src="<?php echo $path; ?>assets/images/numbers-4.png">
+                    <img src="<?php echo $path; ?>assets/images/numbers-4-ar.png">
                 </div>
             </div>
         </div>
@@ -48,20 +47,14 @@ include('../includes/header.php');
             <div class="row">
                 <div class="w-100 row we-care">
                     <div class="col-lg-12 text-center">
-                        <h2 class="ttl-40 fw-bold pt-5 text-uppercase">We care about each one of you</h2>
+                        <h2 class="ttl-40 fw-bold pt-5 text-uppercase">نعتني بكل واحد منكم</h2>
                     </div>
                     <div class="col-lg-7 py-5">
                         <img class="bus-img" src="<?php echo $path; ?>assets/images/school-bus-1.png">
                     </div>
                     <div class="col-lg-5 d-flex align-items-center">
                         <div class="text">
-                            <p>Emirates Transport is dedicated to inclusivity, providing specially designed buses for
-                                the
-                                transportation of students of determination, ensuring a safe and comfortable journey to
-                                and
-                                from school. This initiative reflects our commitment to accessible transportation
-                                solutions
-                                that cater to the unique needs of all students in the UAE.</p>
+                            <p>تكرس شركة الإمارات للنقل جهودها لتعزيز الشمولية، حيث تقدم حافلات مصممة خصيصًا لنقل طلاب العزيمة، مما يضمن رحلة آمنة ومريحة إلى المدرسة ومنه. تعكس هذه المبادرة التزامنا بتوفير حلول نقل ميسرة تلبي الاحتياجات الفريدة لجميع الطلاب في الإمارات.</p>
                         </div>
                     </div>
                 </div>
@@ -74,9 +67,8 @@ include('../includes/header.php');
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <p class="text-gray">Our Technologies</p>
-                    <h3 class="ttl-30 fw-bold text-black text-uppercase">We empower each journey with innovative
-                        technology</h3>
+                    <p class="text-gray">تكنولوجياتنا</p>
+                    <h3 class="ttl-30 fw-bold text-black text-uppercase">Wنحن نُمكّن كل رحلة من خلال التكنولوجيا المبتكرة</h3>
                 </div>
             </div>
         </div>
@@ -84,19 +76,13 @@ include('../includes/header.php');
             <div class="row">
                 <div class="col-lg-3">
                     <div class="col-lg-12 pb-2 pb-lg-5">
-                        <h3 class="fw-bold text-black">Safety-Focused
-                            Drivers</h3>
-                        <p>Our highly trained drivers are committed to safeguarding every student’s journey, paying
-                            close attention to every detail to create a safe, comfortable, and worry-free transportation
-                            experience for students and parents alike.
+                        <h3 class="fw-bold text-black">سائقون يركزون على السلامة</h3>
+                        <p>سائقونا المدربون بعناية ملتزمون بضمان سلامة رحلة كل طالب، مع إيلاء اهتمام بالغ لكل التفاصيل لخلق تجربة نقل آمنة، مريحة وخالية من القلق للطلاب وأولياء الأمور على حد سواء.
                         </p>
                     </div>
                     <div class="col-lg-12 pt-2 pt-lg-5">
-                        <h3 class="fw-bold text-black">Smart Routes for
-                            Seamless Rides</h3>
-                        <p>Using intelligent route-planning technology, we optimize every journey to minimize travel
-                            time and maximize efficiency, guaranteeing students arrive safely and on time while reducing
-                            traffic and environmental impact.
+                        <h3 class="fw-bold text-black">مسارات ذكية لرحلات سلسة</h3>
+                        <p>باستخدام تقنية التخطيط الذكي للمسارات، نقوم بتحسين كل رحلة لتقليل وقت السفر وزيادة الكفاءة إلى أقصى حد، مما يضمن وصول الطلاب بأمان وفي الوقت المحدد، مع تقليل الازدحام المروري والأثر البيئي.
                         </p>
                     </div>
                 </div>
@@ -105,18 +91,13 @@ include('../includes/header.php');
                 </div>
                 <div class="col-lg-3">
                     <div class="col-lg-12 pb-2 pb-lg-5">
-                        <h3 class="fw-bold text-black">Alert
-                            Drivers</h3>
-                        <p>Equipped with cutting-edge technology, our drivers receive instant, real-time notifications,
-                            allowing them to make proactive decisions that enhance road safety and ensure a smooth,
-                            secure journey for all students.
+                        <h3 class="fw-bold text-black">سائقون متيقظون</h3>
+                        <p>مزودون بأحدث التقنيات، يتلقى سائقونا إشعارات فورية وفي الوقت الفعلي، مما يتيح لهم اتخاذ قرارات استباقية تعزز سلامة الطريق وتضمن رحلة سلسة وآمنة لجميع الطلاب.
                         </p>
                     </div>
                     <div class="col-lg-12 pt-2 pt-lg-5">
-                        <h3 class="fw-bold text-black">360° Vision
-                            for Total Security</h3>
-                        <p>Our vehicles are equipped with advanced 360° surveillance systems, providing comprehensive
-                            monitoring to ensure complete security and peace of mind from pick-up to drop-off.</p>
+                        <h3 class="fw-bold text-black">رؤية بزاوية 360° لضمان الأمان الكامل</h3>
+                        <p>مركباتنا مزودة بأنظمة مراقبة متقدمة بزاوية 360°، مما يوفر مراقبة شاملة لضمان الأمان الكامل وراحة البال من لحظة الصعود حتى النزول.</p>
                     </div>
                 </div>
             </div>
@@ -130,9 +111,9 @@ include('../includes/header.php');
             <div class="row h-100">
                 <div class="col-lg-12 h-100 text-center d-flex align-items-center justify-content-center">
                     <div class="text z-index-2">
-                        <h2 class="fw-bold text-white ttl-40">Start your journey now</h2>
+                        <h2 class="fw-bold text-white ttl-40">ابدأ رحلتك الآن</h2>
                         <a href="<?php echo $path; ?>contact/"
-                            class="btn py-3 px-5 btn-outline-light mt-4 fw-bold">CONTACT US</a>
+                            class="btn py-3 px-5 btn-outline-light mt-4 fw-bold">تواصل معنا</a>
                     </div>
                 </div>
             </div>
@@ -144,23 +125,23 @@ include('../includes/header.php');
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <p class="text-gray">Our Technologies</p>
-                    <h2 class="fw-bold text-black ttl-40 text-uppercase pb-4">related apps</h2>
+                    <p class="text-gray">تقنياتنا</p>
+                    <h2 class="fw-bold text-black ttl-40 text-uppercase pb-4">التطبيقات المرتبطة</h2>
                 </div>
                 <div class="col-lg-12">
-                    <div class="w-100 slider-app">
+                <div class="w-100 slider-app-ar  ">
                         <div class="row">
                             <div class="col-lg-7">
                                 <img src="<?php echo $path; ?>assets/images/mobile-app.png" alt="">
                             </div>
                             <div class="col-lg-5 d-flex align-items-center">
                                 <div class="text">
-                                    <h4>ET APPs</h4>
-                                    <h1 class="fw-bold text-uppercase text-primary">Hafilaty</h1>
-                                    <p>Emirates Transport, in collaboration with the Emirates Schools Establishment,
-                                        launched a smart app in early 2023 for parents of public-school students. </p>
-                                    <a href="https://apps.apple.com/ae/app/hafilaty-%D8%AD%D8%A7%D9%81%D9%84%D8%AA%D9%89/id1487966163"
-                                        class="btn btn-primary mt-3"><svg id="Layer_2"
+                                    <h4>تطبيقات ET</h4>
+                                    <h1 class="text-primary fw-bold">حافالتي</h1>
+                                    <p>مواصلات الإمارات، بالتعاون مع مؤسسة مدارس الإمارات،
+                                        أطلقت تطبيقًا ذكيًا مطلع عام 2023 لأولياء أمور طلاب المدارس الحكومية.
+                                    </p>
+                                    <a href="" class="btn btn-primary mt-3"><svg id="Layer_2"
                                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.78 24.3">
                                             <defs>
                                             </defs>
@@ -173,8 +154,7 @@ include('../includes/header.php');
                                                 </g>
                                             </g>
                                         </svg></a>
-                                    <a href="https://play.google.com/store/apps/details?id=ae.abudhabi.wathiq.hafilty_parent&pcampaignid=web_share"
-                                        class="btn btn-primary mt-3">
+                                    <a href="" class="btn btn-primary mt-3">
 
                                         <svg id="Layer_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.3 21.7">
                                             <defs>
@@ -202,16 +182,16 @@ include('../includes/header.php');
                         </div>
                         <div class="row">
                             <div class="col-lg-7">
-                                <img src="<?php echo $path; ?>assets/images/mobile-app-2.png" alt="">
+                                <img src="<?php echo $path; ?>assets/images/mobile-app.png" alt="">
                             </div>
                             <div class="col-lg-5 d-flex align-items-center">
                                 <div class="text">
-                                    <h4>OneET Apps</h4>
-                                    <h1 class="fw-bold text-uppercase text-primary">OneET</h1>
-                                    <!-- <p>Emirates Transport, in collaboration with the Emirates Schools Establishment,
-                                        launched a smart app in early 2023 for parents of public-school students. </p> -->
-                                    <a target="_blank" href="https://apps.apple.com/ae/app/oneet/id1541525393"
-                                        class="btn btn-primary mt-3"><svg id="Layer_2"
+                                    <h4>تطبيقات ET</h4>
+                                    <h1 class="text-primary fw-bold">حافالتي</h1>
+                                    <p>مواصلات الإمارات، بالتعاون مع مؤسسة مدارس الإمارات،
+                                        أطلقت تطبيقًا ذكيًا مطلع عام 2023 لأولياء أمور طلاب المدارس الحكومية.
+                                    </p>
+                                    <a href="" class="btn btn-primary mt-3"><svg id="Layer_2"
                                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.78 24.3">
                                             <defs>
                                             </defs>
@@ -224,9 +204,57 @@ include('../includes/header.php');
                                                 </g>
                                             </g>
                                         </svg></a>
-                                    <a target="_blank"
-                                        href="https://play.google.com/store/apps/details?id=com.mysalesforce.mycommunity.C00D3z000000eGDPEA2.A0OT3z0000008OI6GAM.app1&pcampaignid=web_share"
-                                        class="btn btn-primary mt-3">
+                                    <a href="" class="btn btn-primary mt-3">
+
+                                        <svg id="Layer_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.3 21.7">
+                                            <defs>
+                                                <style>
+                                                    .cls-1 {
+                                                        fill: #d5d8db;
+                                                    }
+                                                </style>
+                                            </defs>
+                                            <g id="Layer_2-2" data-name="Layer_2">
+                                                <g>
+                                                    <path class="cls-1"
+                                                        d="M.19,1.11c-.12.26-.19.55-.19.85v17.78c0,.17.02.35.07.51l8.72-9.59L.19,1.11Z" />
+                                                    <path class="cls-1"
+                                                        d="M12.96,6.06L2.93.26C2.13-.2,1.26,0,.7.46l8.63,9.6,3.63-4Z" />
+                                                    <path class="cls-1"
+                                                        d="M9.33,11.27L.47,21.01c.55.63,1.54.95,2.46.42l10.23-5.91-3.55-3.95-.28-.31Z" />
+                                                    <path class="cls-1"
+                                                        d="M18.33,9.16l-4.64-2.68-3.81,4.19h0l.08.08,3.92,4.36,4.45-2.57c.61-.35.98-.98.98-1.69s-.36-1.34-.98-1.69Z" />
+                                                </g>
+                                            </g>
+                                        </svg></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-7">
+                                <img src="<?php echo $path; ?>assets/images/mobile-app.png" alt="">
+                            </div>
+                            <div class="col-lg-5 d-flex align-items-center">
+                                <div class="text">
+                                    <h4>تطبيقات ET</h4>
+                                    <h1 class="text-primary fw-bold">حافالتي</h1>
+                                    <p>مواصلات الإمارات، بالتعاون مع مؤسسة مدارس الإمارات،
+                                        أطلقت تطبيقًا ذكيًا مطلع عام 2023 لأولياء أمور طلاب المدارس الحكومية.
+                                    </p>
+                                    <a href="" class="btn btn-primary mt-3"><svg id="Layer_2"
+                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.78 24.3">
+                                            <defs>
+                                            </defs>
+                                            <g id="Layer_2-2" data-name="Layer_2">
+                                                <g>
+                                                    <path class="cls-1"
+                                                        d="M13.75,3.51C14.88,2.02,14.78,0,14.78,0c0,0-1.84-.18-3.56,1.8-1.56,1.8-1.3,3.7-1.3,3.7,0,0,1.78.7,3.82-1.99Z" />
+                                                    <path class="cls-1"
+                                                        d="M16.53,13c-.07-2.91,2.4-4.56,2.57-4.73s-1.61-2.16-3.77-2.35c-2.62-.24-4,1-5.43,1.06-1.73.07-2.23-1.44-5.26-.82C2.64,6.57-.33,8.99.03,13.89c.31,4.23,2.85,9.43,5.69,10.35,1.1.36,3.48-1.04,4.64-1.03,1.09,0,3.83,1.47,4.8.98,3.29-1.66,4.61-6.34,4.61-6.34,0,0-3.15-1.06-3.24-4.85Z" />
+                                                </g>
+                                            </g>
+                                        </svg></a>
+                                    <a href="" class="btn btn-primary mt-3">
 
                                         <svg id="Layer_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.3 21.7">
                                             <defs>
@@ -259,7 +287,7 @@ include('../includes/header.php');
     </section>
     <!-- Slider section start -->
 </main>
-<?php include('../includes/footer.php');?>
+<?php include('../../includes/footer-ar.php');?>
 <!-- pagescript here... -->
 <script></script>
 <!-- End pagescript here... -->
